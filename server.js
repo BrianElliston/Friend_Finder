b3931374d620906b791f7a1724736dff748ac061
Brian Elliston <brianelliston@Brians-MacBook-Pro.local>
2018-01-16 15:01:30 -0500

    file structure created

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+
+var app = express();
+var PORT = 3000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//My listener so my server is turned on
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
